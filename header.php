4e15990c3488ec5bbc8faa003d1c28879f55a774
Sam Killermann <sK@hues.xyz>
2019-05-27 15:15:49 -0500

    update favicon

--- header.php
+++ header.php
@@ -18,13 +18,13 @@
 		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-
 		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
-		<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/manifest.json">
+		<link rel="manifest" href=<?php echo get_template_directory_uri(); ?>"/site.webmanifest">
 		<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/safari-pinned-tab.svg" color="#eb366e">
-		<meta name="theme-color" content="#eb366e">
+		<meta name="msapplication-TileColor" content="#000000">
+		<meta name="theme-color" content="#ffffff">
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
